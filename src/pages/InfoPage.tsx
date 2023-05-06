2dfa0116eb4ce01a27f3968e8028c404bba0903b
Mohamed Ishad <ishad.19@cse.mrt.ac.lk>
2023-05-07 04:59:05 +0530

    Format files

--- src/pages/InfoPage.tsx
+++ src/pages/InfoPage.tsx
@@ -1,28 +1,30 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ProfileNavbar from "../components/ProfileNavbar";
 
 
-function InfoPage(){
+function InfoPage() {
     return (<>
         <ProfileNavbar/>
-        <form>
-      <div className="mb-3">
-        <label htmlFor="id" className="form-label">ID</label>
-        <input type="text" className="form-control" id="id"  />
-      </div>
-      <div className="mb-3">
-        <label htmlFor="address" className="form-label">Address</label>
-        <input type="text" className="form-control" id="address" />
-      </div>
-      <div className="mb-3 form-check">
-        <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-        <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-      </div>
-      <div className="d-flex justify-content-center">
-        <button type="submit" className="btn btn-primary">Submit</button>
-      </div>
-    </form>
-        </>);
+        <div className="container p-3">
+            <form>
+                <div className="mb-3">
+                    <label htmlFor="id" className="form-label">ID</label>
+                    <input type="text" className="form-control" id="id"/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="address" className="form-label">Address</label>
+                    <input type="text" className="form-control" id="address"/>
+                </div>
+                <div className="mb-3 form-check">
+                    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+                    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+                </div>
+                <div className="d-flex justify-content-center">
+                    <button type="submit" className="btn btn-outline-success form-control">Submit</button>
+                </div>
+            </form>
+        </div>
+    </>);
 }
 
 export default InfoPage;
