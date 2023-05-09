ae0c3cb893eed4388d2e454958f894b7a45131bb
Mohamed Ishad <ishad.19@cse.mrt.ac.lk>
2023-05-09 17:32:38 +0530

    Add home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import LandingPage from "./pages/LandingPage";
 import React from 'react';
+import {useAuthContext} from "@asgardeo/auth-react";
+import HomePage from "./pages/HomePage";
 
 function App() {
+    const {state} = useAuthContext();
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<LandingPage/>}/>
+                {state &&
+                    <>
+                        <Route path="/home" element={<HomePage/>}/>
+                    </>
+                }
             </Routes>
         </BrowserRouter>
     );
