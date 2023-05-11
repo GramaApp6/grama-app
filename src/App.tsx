b7b661c3a185e33906c3f8074068364a7d5d24ec
Ashok12011234 <asokkumarsusitharan@gmail.com>
2023-05-11 22:03:08 +0530

    props used

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import {useAuthContext} from "@asgardeo/auth-react";
-import ProfilePage from './pages/ProfilePage';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useAuthContext } from "@asgardeo/auth-react";
+import ProfilePage from "./pages/ProfilePage";
+import "./App.css";
 
 import LandingPage from "./pages/LandingPage";
 import HomePage from "./pages/HomePage";
@@ -11,31 +11,55 @@ import SentRequestsPage from "./pages/SentRequestsPage.tsx";
 import Page404 from "./pages/Page404";
 
 function App() {
-    const {state} = useAuthContext();
-    if (state.isLoading) {
-        return (
-            <div className="vh-100 d-flex justify-content-center align-items-center">
-                <h1 className="text-center text-secondary loading">Loading</h1>
-            </div>
-        );
-    }
+  const { state, getBasicUserInfo } = useAuthContext();
+  if (state.isLoading) {
     return (
-        <BrowserRouter>
-            <Routes>
-                {state.isAuthenticated ?
-                    <>
-                        <Route path="/" element={<HomePage/>}/>
-                        <Route path="/request" element={<RequestPage/>}/>
-                        <Route path="/requests" element={<SentRequestsPage/>}/>
-                        <Route path="/profile" element={<ProfilePage/>}/>
-                    </>
-                    :
-                    <Route path="/" element={<LandingPage/>}/>
-                }
-                <Route path="*" element={<Page404/>}/>
-            </Routes>
-        </BrowserRouter>
+      <div className="vh-100 d-flex justify-content-center align-items-center">
+        <h1 className="text-center text-secondary loading">Loading</h1>
+      </div>
     );
+  }
+
+  const [display_name, setDisplayName] = useState(String);
+  const [picture, setPicture] = useState(String);
+  const [full_name, setFullName] = useState(String);
+  const [role, setRole] = useState(String);
+  const [email, setEmail] = useState(String);
+
+  useEffect(() => {
+    getBasicUserInfo()
+      .then((response : Record<string, string>) => {
+        setDisplayName(response["displayName"]);
+        setPicture(response["picture"]);
+        setFullName(response["name"]);
+        setRole(response["groups"]);
+        setEmail(response["email"]);
+
+        console.log(response);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        {state.isAuthenticated ? (
+          <>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/request" element={<RequestPage />} />
+            <Route path="/requests" element={<SentRequestsPage />} />
+            <Route path="/profile" element={<ProfilePage email={email} role={role} full_name={full_name} picture={picture} display_name={display_name} />} />
+
+          </>
+        ) : (
+          <Route path="/" element={<LandingPage />} />
+        )}
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,21 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import ProfileNavbar from "../components/ProfileNavbar";
-import { useAuthContext } from "@asgardeo/auth-react";
 
-function ProfilePage() {
-  const { getBasicUserInfo } = useAuthContext();
-  const [user_attribute, setAttributeList] = useState<Record<string, string>>();
 
-  useEffect(() => {
-    getBasicUserInfo()
-      .then((response) => {
-        setAttributeList(response);
-        console.log(response);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
+function ProfilePage(props: any) {
+  
+  
 
   return (
     <>
@@ -24,19 +13,19 @@ function ProfilePage() {
       <div className="container">
         <div className="row justify-content-md-center mb-3">
           <div className="col-md-auto">
-            {user_attribute && (
+
               <img
-                src={user_attribute["picture"]}
+                src={props.picture}
                 className="rounded-circle"
                 width="200" // set the width to 200 pixels
                 height="200" // set the height to 200 pixels
               />
-            )}
+          
           </div>
         </div>
         <div className="row mt-3 justify-content-md-center">
           <div className="col-md-auto">
-            {user_attribute && <h1>{user_attribute["displayName"]}</h1>}
+            <h1>{props.display_name}</h1>
           </div>
         </div>
         <ul className="list-group">
@@ -46,16 +35,16 @@ function ProfilePage() {
                 <h4 className="text-muted">Full name </h4>
               </div>
               <div className="col">
-                {user_attribute && (
+               
                   <input
                     type="text"
                     className="form-control"
-                    placeholder={user_attribute["name"]}
+                    placeholder={props.full_name}
                     aria-label="Disabled input example"
                     disabled
                     readOnly
                   />
-                )}
+             
               </div>
             </div>
           </li>
@@ -65,16 +54,16 @@ function ProfilePage() {
                 <h4 className="text-muted">Email </h4>
               </div>
               <div className="col">
-                {user_attribute && (
+              
                   <input
                     type="text"
                     className="form-control"
-                    placeholder={user_attribute["email"]}
+                    placeholder={props.email}
                     aria-label="Disabled input example"
                     disabled
                     readOnly
                   />
-                )}
+                
               </div>
             </div>
           </li>
@@ -84,16 +73,16 @@ function ProfilePage() {
                 <h4 className="text-muted">Role </h4>
               </div>
               <div className="col">
-                {user_attribute && (
+               
                   <input
                     type="text"
                     className="form-control"
-                    placeholder={user_attribute["groups"]}
+                    placeholder={props.role}
                     aria-label="Disabled input example"
                     disabled
                     readOnly
                   />
-                )}
+              
               </div>
             </div>
           </li>
