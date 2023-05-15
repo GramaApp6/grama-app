042f2fc2ee315a08224f5fa4e422377e86af69f1
sandun induwara nishshanka <sanduninduwara2@gmail.com>
2023-05-16 00:05:44 +0530

    front end fix v1

--- src/pages/RequestInfo.tsx
+++ src/pages/RequestInfo.tsx
@@ -1,13 +1,13 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import ProfileNavbar from "../components/ProfileNavbar";
 
-import {Address, GramaCertificate} from "../types";
-import {Status} from "../types/GramaCertificate.ts";
-import {getAddressAsString} from "../utils/commonUtils.ts";
-import {useParams} from "react-router-dom";
+import { Address, GramaCertificate } from "../types";
+import { Status } from "../types/GramaCertificate.ts";
+import { getAddressAsString } from "../utils/commonUtils.ts";
+import { useParams } from "react-router-dom";
 
 const RequestInfo = () => {
-    const {id} = useParams();
+    const { id } = useParams();
     const [request, setRequest] = useState<GramaCertificate>(
         {
             address: {
@@ -26,11 +26,11 @@ const RequestInfo = () => {
             reason: "",
             requestDate: new Date(),
             status: Status.NEW,
-            validationStatus: {address: Status.NEW, identity: Status.NEW, police: Status.NEW}
+            validationStatus: { address: Status.NEW, identity: Status.NEW, police: Status.NEW }
         }
     );
 
-    useEffect(()=>{
+    useEffect(() => {
         //TODO: fetch data from backend based on requestID
         // setRequest();
     }, [id])
@@ -45,60 +45,91 @@ const RequestInfo = () => {
 
     return (
         <>
-            <ProfileNavbar/>
+            <ProfileNavbar />
             <div className="container">
-                <div className="text-center">
+                <div className="text-center mt-1 mb-4">
                     <h1>User Details</h1>
                 </div>
-                <div className="card text-start">
+                <div className="card text-start shadow-lg p-3 mb-5 bg-body rounded">
                     <div className="card-body">
-                        <h6 className="card-subtitle mb-2 text-muted">Name</h6>
-                        <h5 className="card-title">{request?.firstName + " " + request?.lastName}</h5>
-                        <hr/>
-                        <h6 className="card-subtitle mb-2 text-muted">NIC</h6>
-                        <h6 className="card-subtitle mb-2 text-muted">{request?.nic}</h6>
-                        <hr/>
-                        <h6 className="card-subtitle mb-2 text-muted">Address</h6>
-                        <p className="card-text">{getAddressAsString(request?.address as Address)}</p>
-                        <hr/>
-                        <h6 className="card-subtitle mb-2 text-muted">Status</h6>
-                        <div>
-                            <p className="card-text">
-                                Identity - {Status[request.validationStatus.identity]}
-                            </p>
-                            <button className="btn btn-outline-primary">Check</button>
-                        </div>
-                        <div>
-                            <p className="card-text">
-                                 Police - {Status[request.validationStatus.police]}
-                            </p>
-                            <button className="btn btn-outline-primary">Check</button>
-                        </div>
-                        <div>
-                            <p className="card-text">
-                                Address - {Status[request.validationStatus.address]}
-                            </p>
-                            <button className="btn btn-outline-primary">Check</button>
-                        </div>
-                        <hr/>
-                        <div className="text-center">
+                        <h6 className="card-text">Name:</h6>
+                        <h6 className="card-subtitle text-muted">{request?.firstName + " " + request?.lastName}Kumara</h6>
 
-                            <form onSubmit={handleSubmit}>
-                                {request.status === Status.NEW && (
-                                <button
-                                    className="btn btn-success"
-                                    onClick={handleApprove}
-                                >
-                                    Approve
-                                </button>
+                        <hr className="hr-success" />
 
-                            )}
-                                <button className="btn btn-success" type="submit">
-                                    Submit
-                                </button>
-                                <a className="btn btn-danger" href="/requests">
-                                    Cancel
+                        <h6 className="card-text">NIC:</h6>
+                        <h6 className="card-subtitle mb-2 text-muted">{request?.nic}1999</h6>
+                        <hr  className="hr-success"/>
+                        
+                        <h6 className="card-text">Address:</h6>
+                        <h6 className="card-subtitle mb-2 text-muted">houseNumber</h6>
+                        <h6 className="card-subtitle mb-2 text-muted">streetName</h6>
+                        <h6 className="card-subtitle mb-2 text-muted">suburb</h6>
+
+                        <hr   className="hr-success"/>
+
+
+                        <h6 className="card-text">
+                            Check Identity:
+                        </h6>
+                        <div className="d-flex justify-content-between mb-3">
+                            <h6 className="card-subtitle mb-2 text-muted  my-auto">{Status[request.validationStatus.identity]}</h6>
+                            <button className="btn btn-outline-primary inline ms-5">Check</button>
+                        </div>
+
+                        <h6 className="card-text">
+                            Check Police:
+                        </h6>
+                        <div className="d-flex justify-content-between mb-3">
+                            <h6 className="card-subtitle mb-2 text-muted  my-auto">{Status[request.validationStatus.police]}</h6>
+
+                            <button className="btn btn-outline-primary inline ms-5">Check</button>
+                        </div>
+
+                        <h6 className="card-text ">
+                            Check Address:
+                        </h6>
+                        <div className="d-flex justify-content-between mb-3">
+                            <h6 className="card-subtitle mb-2 text-muted  my-auto">{Status[request.validationStatus.address]}</h6>
+
+                            <button className="btn btn-outline-primary  ms-5">Check</button>
+                        </div>
+                        <hr  className="hr-success"/>
+
+                        <div className="d-flex justify-content-between">
+                            <h6 className="card-text my-auto">Set Status:</h6>
+
+                            <div class="dropdown">
+                                <a class="btn btn-outline-primary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Dropdown
                                 </a>
+
+                                <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+
+
+
+                        <div className="text-center mt-5 ">
+
+
+                            <form onSubmit={handleSubmit} className="d-grid gap-2">
+                                {request.status === Status.NEW && (
+                                    <button
+                                        className="btn btn-lg btn-success"
+                                        onClick={handleApprove}
+                                    >
+                                        Submit
+                                    </button>
+
+                                )}
+
+
                             </form>
                         </div>
                     </div>
