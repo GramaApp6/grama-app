faf6fa096487e476196824252dc8357492263a34
Mohamed Ishad <ishad.19@cse.mrt.ac.lk>
2023-06-04 21:25:03 +0530

    Move node-hook url to constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,2 @@
 export const url = "https://c797a448-6b78-43cc-b089-fcc4e8df8937-dev.e1-us-east-azure.choreoapis.dev/yjoh/gramaappbackend/endpoint-9091-6c0/1.0.0";
+export const node_hook_url = "https://c797a448-6b78-43cc-b089-fcc4e8df8937-dev.e1-us-east-azure.choreoapis.dev/yjoh/nodehook/node-hook-197/1.0.0";
